51b468c03a2d60b13c1ef9a86d4302b549761c8a
tiantangyu69 <304663257@qq.com>
2016-04-08 18:06:32 +0800

    typeof

--- js/typeof.js
+++ js/typeof.js
@@ -12,3 +12,40 @@ console.log(typeof f);
 console.log(typeof null);
 console.log(typeof undefined);
 console.log(typeof true);
+
+// 块语句没有块级作用域，申明的变量并不是块级私有的
+{
+    var z = "this is block variable";
+}
+
+console.log(z);
+
+console.log("========================================================");
+
+var arr = new Array();
+arr.push("aa");
+arr.push("bb");
+arr.push("cc");
+arr.push("dd");
+
+console.log(Array.prototype.forEach);
+
+// IE不能兼容，IE中没有该方法
+arr.forEach(function (ele) {
+    console.log(ele);
+})
+
+for (var i in arr) {
+    console.log(arr[i]);
+}
+
+var input = prompt("请输入数字：", 0);
+console.log(input);
+
+console.log("strict模式=================================+");
+var hasStrictMode = (function () {
+    "use strict";
+    return this == undefined;
+})();
+console.log(hasStrictMode);
+
